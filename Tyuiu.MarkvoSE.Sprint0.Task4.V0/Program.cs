d16588a233c90f51ff2877e30d79c92e0df6f846
Tyuiu.MarkovSE@outlook.com <Tyuiu.MarkovSE@outlook.com>
2024-09-15 21:39:21 +0500

    Task4

---
+++ Tyuiu.MarkvoSE.Sprint0.Task4.V0/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.MarkvoSE.Sprint0.Task4.V0.Lib;
+
+namespace Tyuiu.MarkvoSE.Sprint0.Task4.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Пример линейной структуры программы
+            //Вызов метода сложения Addition
+            Console.WriteLine(DataService.Addition(1, 5));
+        
+            Console.ReadKey();
+    }
+}
