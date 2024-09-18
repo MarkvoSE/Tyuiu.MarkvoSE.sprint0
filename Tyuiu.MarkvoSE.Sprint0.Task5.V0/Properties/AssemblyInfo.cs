4040eb58351842f014bb3ad02e4e5cc132ae523a
Tyuiu.MarkovSE@outlook.com <Tyuiu.MarkovSE@outlook.com>
2024-09-18 19:47:39 +0500

    task5

---
+++ Tyuiu.MarkvoSE.Sprint0.Task5.V0/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.MarkvoSE.Sprint0.Task5.V0")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.MarkvoSE.Sprint0.Task5.V0")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f4504a7a-45c0-411f-96ac-1bdc53b98f49")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
